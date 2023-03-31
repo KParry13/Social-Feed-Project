73fc5a14d6cdba6428454026dcbef1e18863ff8d
Kayla <k_meijer_10@yahoo.com>
2023-03-31 17:11:17 -0400

    created Post functionality file

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -7,11 +7,28 @@
 // Bind these functions to their respective buttons onClick event. 
 // Within these functions, start to lay out the steps that need to occur when each button is clicked.
 
+
+
 const Posts = (props) => {
+
     return ( 
         <table className="table">
-            <tr>Name</tr>
-            <tr>Post</tr>
+            <thead>
+                <tr>Name</tr>
+                <tr>Post</tr>
+            </thead>
+            <tbody>
+                {props.parentEntries.map((entry, index) => {
+                    return (
+                        <tr key={index}>
+                            <td>{index + 1}</td>
+                            <td>{entry.name}</td>
+                            <td>{entry.post}</td>
+                        </tr>
+                        
+                    )
+                })}
+            </tbody>
         </table>
      );
 }
